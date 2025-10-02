f70bf3b08015f20e07c245a5eb677bf0a517fad1
Arunava Gantait <agantait@inria.fr>
2025-10-02 16:38:24 +0200

    bunch of deleted stuff

--- elpa/ox-typst-20250704.1835/ox-typst-pkg.el
+++
@@ -1,9 +0,0 @@
-;; -*- no-byte-compile: t; lexical-binding: nil -*-
-(define-package "ox-typst" "20250704.1835"
-  "Typst Back-End for Org Export Engine."
-  '((emacs "30.1")
-    (org   "9.7"))
-  :url "https://github.com/jmpunkt/ox-typst"
-  :commit "5c855902fe11c10292e495501e7f1b4adbb53d16"
-  :revdesc "5c855902fe11"
-  :keywords '("text" "wp" "org" "typst"))
