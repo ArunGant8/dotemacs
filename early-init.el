4f04d91d6eb2ac4413b2c0eb6a38c42665fb8fef
Arunava Gantait <agantait@inria.fr>
2025-11-03 17:52:30 +0100

    made early-init more robust

--- early-init.el
+++ early-init.el
@@ -1,2 +1,6 @@
-(use-package org
-  :load-path "~/.emacs.d/elpa/org-mode/lisp/")
+(unless (file-exists-p "~/.emacs.d/elpa/org-mode/lisp/")
+  (package-vc-install '(org-mode :url "https://code.tecosaur.net/tec/org-mode" :branch "dev")))
+
+(when (file-exists-p "~/.emacs.d/elpa/org-mode/lisp/")
+  (use-package org
+    :load-path "~/.emacs.d/elpa/org-mode/lisp/"))
