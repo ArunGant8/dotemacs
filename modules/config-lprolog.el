2ef4f3c5a6515d4105dea92f959abf6d42910d92
Arunava Gantait <agantait@inria.fr>
2025-11-17 17:56:53 +0100

    added Teyjus path to Emacs

--- modules/config-lprolog.el
+++ modules/config-lprolog.el
@@ -1,3 +1,6 @@
 (load "~/Projects/teyjus/emacs/teyjus.el")
 
+;; Don't know why Emacs is unable to find this
+(setq tjcc "~/Projects/teyjus/_build/install/default/bin/tjcc")
+
 (provide 'config-lprolog)
