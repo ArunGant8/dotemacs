d98db0624e64e8fa2e84afbdee4d536fe36bf3c3
Arunava Gantait <agantait@inria.fr>
2025-12-09 15:15:16 +0100

    updated stuff

--- transient/history.el
+++ transient/history.el
@@ -1,7 +1,12 @@
 ((casual-calc-tmenu nil)
  (magit-commit nil)
+ (magit-log
+  ("-n256" "--graph" "--decorate"))
  (magit-pull nil)
- (magit-push nil)
+ (magit-push
+  ("--force-with-lease")
+  nil)
+ (magit-reset nil)
  (magit-revert
   ("--edit"))
  (org-ref-citation-menu nil)
