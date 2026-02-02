89fc6f97b03db1c37a6da0ac66bffeb0f45ea373
Arunava Gantait <agantait@inria.fr>
2026-02-03 00:28:52 +0100

    updated stuff

--- custom.el
+++ custom.el
@@ -3,6 +3,10 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(erc-modules
+   '(autojoin button completion fill imenu irccontrols list match menu
+	      move-to-prompt netsplit networks readonly ring sasl
+	      stamp track))
  '(package-selected-packages
    '(auctex casual cdlatex citar citar-org-roam consult counsel dune
 	    easysession elfeed elfeed-dashboard elfeed-goodies
