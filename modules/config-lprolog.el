a282f3f6087fe7e14fd7d589b3620d5ef6b7e59a
Arunava Gantait <agantait@inria.fr>
2026-04-10 16:24:32 +0200

    added a function to toggle between a .mod and .sig file

--- modules/config-lprolog.el
+++ modules/config-lprolog.el
@@ -3,4 +3,18 @@
 ;; Don't know why Emacs is unable to find this
 (setq tjcc "~/Projects/teyjus/_build/install/default/bin/tjcc")
 
+(defun my/lp-toggle-between-mod-and-sig (file)
+  "Toggle between a .mod file and its corresponding
+  .sig file in a lambdaProlog project"
+  (interactive (list (file-name-nondirectory (buffer-file-name))))
+  (let ((filename (file-name-sans-extension file))
+	(extension (file-name-extension file)))
+    (cond
+     ((string-equal extension "mod")
+      (find-file (concat filename ".sig")))
+     ((string-equal extension "sig")
+      (find-file (concat filename ".mod"))))))
+
+(keymap-global-set "C-c p t" #'my/lp-toggle-between-mod-and-sig)
+
 (provide 'config-lprolog)
