b9584acabe6db74db22f7290b56349f35149b8db
Arunava Gantait <agantait@inria.fr>
2026-04-15 16:02:18 +0200

    minor changes to ai config. set default mode to Org

--- modules/config-ai.el
+++ modules/config-ai.el
@@ -1,15 +1,18 @@
 ;; AI from within Emacs using GPTel
 
 (use-package gptel
-  :ensure t)
+  :ensure t
+  :config
+  (setq
+   gptel-model   'test
+   gptel-backend (gptel-make-openai "llama-cpp"
+                   :stream t
+                   :protocol "http"
+                   :host "localhost:8001"
+		   :header (lambda () '())
+                   :models '(test))
+   gptel-default-mode 'org-mode))
 
-(setq
- gptel-model   'test
- gptel-backend (gptel-make-openai "llama-cpp"
-                 :stream t
-                 :protocol "http"
-                 :host "localhost:8001"
-		 :header (lambda () '())
-                 :models '(test)))
+
 
 (provide 'config-ai)
